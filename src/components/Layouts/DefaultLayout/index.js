c8296278296eddc67a4cd799d3a1f9b54c090ccf
datngth03 <datngth03@gmail.com>
2023-10-06 21:44:56 +0700

    #

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,17 @@
 import Header from '../components/Header';
 import Slidebar from './Slidebar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
    return (
-      <div>
+      <div className={cx('wrapper')}>
          <Header />
-         <div className="container">
+         <div className={cx('container')}>
             <Slidebar />
-            <div className="content">{children}</div>
+            <div className={cx('content')}>{children}</div>
          </div>
       </div>
    );
